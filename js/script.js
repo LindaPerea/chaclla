43d3034421e4e066b205c4961d92fc86ee1b68f8
LindaPerea <linda.hp@hotmail.com>
2023-03-18 01:59:05 -0500

    second commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var menuToggle = document.querySelector('.menu-toggle');
+var menu = document.querySelector('.menu');
+
+menuToggle.addEventListener('click', function() {
+  menu.classList.toggle('active');
+  menuToggle.classList.toggle('active');
+});
